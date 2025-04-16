c6fb10d4168ab327677232fdddf17e5eda0369ff
karina <tjutkarinaaisyairah1999@gmail.com>
2025-04-16 11:04:52 +0700

    integrate template

---
+++ database/migrations/2025_03_12_055343_create_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('categories_id')->references('id')->on('categories');
+            $table->string('name');
+            $table->integer('price');
+            $table->integer('stok');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
